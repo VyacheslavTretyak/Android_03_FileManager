3f53b1e7e052d7a547a4cbc618c179170f976e1f
vtretyak <tretyak1c@gmail.com>
2019-09-06 13:41:01 +0300

    upd: add copy, paste, delete and rename

--- app/src/main/java/com/homework/android_03_filesmanager/MainActivity.java
+++ app/src/main/java/com/homework/android_03_filesmanager/MainActivity.java
@@ -11,17 +11,25 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,6 +41,11 @@ public class MainActivity extends AppCompatActivity {
     private FileAdapter adapter;
     private ArrayList<FileView> listFiles;
     private File path;
+    private File copiedFile;
+    private AlertDialog dialogYesNo;
+    private AlertDialog dialogCreate;
+    private AlertDialog dialogRename;
+    private File esMainDir;
 
     public Bitmap directoryPic;
     public Bitmap filePic;
@@ -71,27 +84,71 @@ public class MainActivity extends AppCompatActivity {
         });
 
         AlertDialog.Builder builder = new	AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Dialog_Alert);
-        builder.setMessage("Are you sure?");
-        builder.setTitle("Warning");
+        builder.setTitle("Create directory");
+        LayoutInflater inflater	= this.getLayoutInflater();
+        final View view	= inflater.inflate(R.layout.create_dir_layout,null,false);
+        builder.setView(view);
         builder.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
             public void onClick(DialogInterface dialog, int id) {
-
+                EditText editText = view.findViewById(R.id.edit_create_dir);
+                File f = new File(path.getPath()+"//"+editText.getText());
+                f.mkdir();
+                adapter.add(new FileView(f, directoryPic));
             }
         });
         builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                File f = path.getParentFile();
-                if(f != null){
-                    path = f;
-                }
+
             }
         });
-       AlertDialog dialog = builder.create();
-       dialog.setCancelable(false);
+        dialogCreate = builder.create();
+
+        builder = new	AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Dialog_Alert);
+        builder.setTitle("Rename file");
+        LayoutInflater inflater1	= this.getLayoutInflater();
+        final View view1 = inflater.inflate(R.layout.create_dir_layout,null,false);
+        builder.setView(view);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int id) {
+                EditText editText = view.findViewById(R.id.edit_create_dir);
+                File f = adapter.getItem(curItem).file;
+                File newfile = new File(path, editText.getText().toString());
+                f.renameTo(newfile);
+                LoadFiles(path);
+                adapter.notifyDataSetChanged();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        dialogRename = builder.create();
+
+
+
+        builder = new	AlertDialog.Builder(this, android.R.style.Theme_DeviceDefault_Dialog_Alert);
+        builder.setMessage("Are you sure?");
+        builder.setTitle("Warning");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int id) {
+                Delete(adapter.getItem(curItem).file);
+                LoadFiles(path);
+                adapter.notifyDataSetChanged();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+       dialogYesNo = builder.create();
        //dialog.show();
 
-        File esMainDir = null;
+        esMainDir = null;
         if	(this.isExternalStorageWritable())  {
             esMainDir = Environment.getExternalStorageDirectory();
         }
@@ -120,6 +177,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void LoadFiles(File file){
         listFiles.clear();
+        Button button = findViewById(R.id.button_back);
+        if(file.getPath().equals(esMainDir.getPath())){
+            button.setEnabled(false);
+        }else{
+            button.setEnabled(true);
+        }
         File[] arrFiles = file.listFiles();
         for (File f : arrFiles){
             if (f.isDirectory()) {
@@ -144,6 +207,42 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void Paste(File file, File path) throws IOException {
+        if(file == null){
+            return;
+        }
+        if(file.isDirectory()){
+            File d = new File(path.getPath()+"//"+file.getName());
+            d.mkdir();
+            File[] files = file.listFiles();
+            for(File f:files) {
+                Paste(f, d);
+            }
+        } else {
+            Copy(file, path);
+        }
+    }
+
+    public void Copy(File src, File dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        try {
+            OutputStream out = new FileOutputStream(dst);
+            try {
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+            } finally {
+                out.close();
+            }
+        } finally {
+            in.close();
+        }
+    }
+
+
     private static String getFileExtension(File file) {
         String fileName = file.getName();
         if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
@@ -161,6 +260,21 @@ public class MainActivity extends AppCompatActivity {
         return (state.equals(Environment.MEDIA_MOUNTED) || state.equals(Environment.MEDIA_MOUNTED_READ_ONLY));
     }
 
+    private void Delete(File file){
+        //adapter.remove(adapter.getItem(curItem));
+        if(file.isDirectory()){
+            File[] files = file.listFiles();
+            if(files.length < 1){
+                file.delete();
+            }
+            for(File f:files){
+                Delete(f);
+                f.delete();
+            }
+        }
+        file.delete();
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
@@ -173,16 +287,25 @@ public class MainActivity extends AppCompatActivity {
         switch (id) {
 
             case R.id.action_create_directory:
-
+                dialogCreate.show();
                 break;
             case R.id.action_copy:
-
+                copiedFile = adapter.getItem(curItem).file;
                 break;
             case R.id.action_paste:
+                try {
+                    Paste(copiedFile, path);
+                    LoadFiles(path);
+                    adapter.notifyDataSetChanged();
+                }catch (Exception ex){
+                    Toast.makeText(this, ex.getMessage(),Toast.LENGTH_SHORT);
+                }
                 break;
             case R.id.action_delete:
+                dialogYesNo.show();
                 break;
             case R.id.action_rename:
+                dialogRename.show();
                 break;
         }
         return super.onOptionsItemSelected(item);
